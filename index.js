a88444fa19c5faf4d0fadc5d626e41cd6b012e55
NoahWhiffen <Noah.Whiffen@keyin.com>
2025-01-19 22:56:26 -0330

    Added cli functionality and some comments for things left to do

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 // Author: Noah Whiffen - SD12
 // Dates:  January 17th, 2025 - ?
 
-// Create a function to generate the password based on provided length.
-
 const program = require('process'); 
-const arguments = process.argv.slice(2); 
+const arguments = process.argv.slice(2); // Slice off node and script name.
 
 // All possible characters for passwords.
 const alpha = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 const symbols = '!@#$%^&*()_-+=;:?.,<>';
 const upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const validOptions = ['length', 'numbers', 'uppercase', 'symbols'];
 
 // Default values for flags.
 let length = 8;
@@ -40,13 +39,46 @@ function generatePassword(length, includeNumbers, includeUpper, includeSymbols) 
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
     }
+    // Need to add these features on a seperate branch!
 
     return password;
 }
 
-generatePassword();
+// Display help message.
 
+// Parse CLI arguments and set flags.
 
+arguments.forEach((arg, index) => {
+    if (arg === '--length' && arguments[index + 1]) {
+        length = parseInt(arguments[index + 1], 10);
+    } 
+    
+    else if (arg === '--numbers') {
+        includeNumbers = true;
+    } 
+    
+    else if (arg === '--uppercase') {
+        includeUpper = true;
+    } 
+    
+    else if (arg === '--symbols') {
+        includeSymbols = true;
+    } 
+    
+    else {
+        console.log(`Invalid flag or value: ${arg}`);
+    }
+});
+
+// Error handling for invalid length value
+if (isNaN(length) || length <= 0) {
+    console.log("Please provide a valid length greater than 0 using -l <length>.");
+    process.exit(1);
+}
+
+// Generate and display the password.
+const password = generatePassword(length, includeNumbers, includeUpper, includeSymbols);
+console.log("Generated Password:", password);
 
 // accept flags through command line
 
